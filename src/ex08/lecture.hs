d058bceb13f3617ea642bef1c6e90e82f24eaf4d
Andrew Gray <adsgray@gmail.com>
2014-12-04 21:08:12 -0800

    lecture

---
+++ src/ex08/lecture.hs
@@ -0,0 +1,12 @@
+-- 1.
+-- The IO Monad!
+
+
+-- IO Char -- the type of functions that return a character
+-- IO () -- the type of purely side effecting actions that return no result value
+
+-- getChar :: IO Char
+-- putChar :: Char -> IO ()
+-- return :: a -> IO a
+
+-- 2.
